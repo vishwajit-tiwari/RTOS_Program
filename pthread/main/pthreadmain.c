55c1b7b690c7f77c138a314b97d49e502b7d9b9b
Vishwajit1997 <vishwajitkumar37@gmail.com>
2022-01-24 18:00:35 +0530

    Added Thread program

--- pthread/main/pthreadmain.c
+++ pthread/main/pthreadmain.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 #include<pthread.h>
+#include<semaphore.h>
 
-//Function for Posix-Thread
+sem_t s1, s2;   //Semaphore arguments
+
+//Function for Posix-Thread-1
 
 void *helloESP32Thread(void *arg)
 {
-    printf("Inside Thread Function!\n");
+    while(1)
+    {
+        sem_wait(&s1);
+        printf("Inside 1st Thread...\n");
+        printf("Enter two numbers: ");
+        scanf("%d %d", &num1, &num2);
+    }
     return NULL;
 }
 
+//Function for Posix-Thread-1
+
+void *hiESP32Thread(void *arg)
+{
+    while(1)
+    {
+    printf("Inside 2nd Thred...\n");
+    }
+    return NULL;
+}
+
+
 void app_main()
 {
-    pthread_t thread_id;
+    pthread_t thread_id1, thread_id2;
 
-    printf("Hello C-DAC\n");
+    printf("Hi Welcome to thread programming....\n");
 
 //To create Posix-Thread
 
-    pthread_create(&thread_id, NULL, helloESP32Thread, NULL);
-    pthread_join(thread_id, NULL);
+    pthread_create(&thread_id1, NULL, helloESP32Thread, NULL);
+    pthread_create(&thread_id2, NULL, hiESP32Thread, NULL);
+    
+    pthread_join(thread_id1, NULL);
+    pthread_join(thread_id2, NULL);
+
 }
 
